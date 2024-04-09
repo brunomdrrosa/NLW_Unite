6cc5f0585d260eac637927a80d7604a3c7da0d0e
Bruno Machado <brunomdr46@gmail.com>
2024-04-08 21:55:38 -0300

    Aula 1 - O início da sua especialização

---
+++ src/main/java/rocketseat/com/passin/domain/event/Event.java
@@ -0,0 +1,28 @@
+package rocketseat.com.passin.domain.event;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "events")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Event {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String details;
+
+    @Column(nullable = false, unique = true)
+    private String slug;
+
+    @Column(nullable = false, name = "maximum_attendees")
+    private Integer maximumAttendees;
+}
